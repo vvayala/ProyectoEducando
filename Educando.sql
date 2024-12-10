eb30863b52f6623d594d609d9af8378bfee14581
vvayala <vilicayala@gmail.com>
2024-12-09 20:47:30 -0600

    Agregando sql + mainWindow
    Agregando sql

---
+++ Educando.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE Educando;
+USE Educando;
+
+CREATE TABLE Usuarios (
+    IDUsuario INT IDENTITY(1,1) PRIMARY KEY,
+    Usuario NVARCHAR(50) NOT NULL,
+    Contrasena NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(100) UNIQUE,
+);
+
+CREATE TABLE Lecciones (
+    IDLeccion INT IDENTITY(1,1) PRIMARY KEY,
+    NombreLeccion NVARCHAR(255) NOT NULL,
+    Contenido NVARCHAR(MAX),
+);
+
+CREATE TABLE LeccionesCompletadas (
+    IDCompletado INT IDENTITY(1,1) PRIMARY KEY,
+    IDUsuario INT NOT NULL,
+    IDLeccion INT NOT NULL,
+    Tiempo TIME,
+	TotalPreguntas INT,
+    RespuestasCorrectas INT,
+    FOREIGN KEY (IDUsuario) REFERENCES Usuarios(IDUsuario),
+    FOREIGN KEY (IDLeccion) REFERENCES Lecciones(IDLeccion)
+);
+
+CREATE TABLE Preguntas (
+    IDPregunta INT IDENTITY(1,1) PRIMARY KEY,
+    IDLeccion INT NOT NULL,
+    Pregunta NVARCHAR(MAX),
+    FOREIGN KEY (IDLeccion) REFERENCES Lecciones(IDLeccion)
+);
+
+CREATE TABLE Opciones (
+    IDOpcion INT IDENTITY(1,1) PRIMARY KEY,
+    IDPregunta INT NOT NULL,
+    Opcion NVARCHAR(255),
+    EsCorrecta BIT,
+    FOREIGN KEY (IDPregunta) REFERENCES Preguntas(IDPregunta)
+);
+
+
--- mainWindow.py
+++ mainWindow.py
@@ -1,10 +1,15 @@
 import customtkinter as ctk
 import pyodbc
+from tkinter import messagebox
 
-
-def bd():
-    connection = pyodbc.connect('Driver={SQL Server};' 'Server=.\SQLEXPRESS;' 'Database=ITCAPlus;' 'Trusted_connection=yes;')
-    if connection:
-        print(True)
-bd()
+def bd(usuario, contrasena):
+    try:
+        connection = pyodbc.connect('Driver={SQL Server};' 'Server=.\SQLEXPRESS;' 'Database=Educando;' 'Trusted_connection=yes;')
+        cursor = connection.cursor() 
+        cursor.execute("SELECT * FROM Usuarios WHERE Usuario = ? AND Contrasena = ?", (usuario, contrasena)) 
+        result = cursor.fetchone() 
+        return result is not None 
+    except pyodbc.Error as e: 
+        messagebox.showerror("Error", f"Error en la conexión: {e}") 
+        return
 
