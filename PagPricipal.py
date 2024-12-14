820d1fc41fad0e1c89c1edec195a04b99eca8c86
moisescorado91 <coradomoises91@gmail.com>
2024-12-14 08:46:45 -0600

    ventana principal

--- PagPricipal.py
+++ PagPricipal.py
@@ -118,7 +118,8 @@ def leer_contenido_archivo(ruta):
 
 def mostrar_cuento(cuento, usuario):
     ventana_cuento = tk.Toplevel()
-    ventana_cuento.title(cuento['titulo'])ventana_cuento.resizable(False,False)
+    ventana_cuento.title(cuento['titulo'])
+    ventana_cuento.resizable(False,False)
     ventana_cuento.geometry("600x600")
 
     titulo_label = tk.Label(ventana_cuento, text=f"Título: {cuento['titulo']}", font=("Arial", 16, "bold"), wraplength=500)
